34848c098ce4032ebb228224b1083c45212d5ce8
Yuya Imoto <yuya@yuya-MacBookPro.local>
2020-05-13 18:15:43 +0900

    実験刺激(ストループテスト)を追加した

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function input(){
     localStorage.setItem('id', input_form.id.value);
     localStorage.setItem('name', input_form.name.value);
-    location.href = "./stimulus.html";
+    location.href = "./countdown.html";
 }
 
 function output(){
@@ -20,8 +20,44 @@ function countdown(idname){
         var counter = 3 - i;
         obj.textContent = counter;
         if (counter < 0) {
-            location.href = "./index.html";
+            location.href = "./stimulus.html";
         }
         i++;
     }, 1000);
+}
+
+function getColor() {
+    min = Math.ceil(0);
+    max = Math.floor(3);
+    random_num = Math.floor(Math.random() * (max - min)) + min;
+    if (random_num == 0) {
+        return "#ff0000";
+    } else if (random_num == 1) {
+        return "#00ff00";
+    } else if (random_num == 2) {
+        return "#0000ff";
+    } else {
+        return "#ffffff";
+    }
+}
+
+function runStimulus(idname) {
+    var obj = document.getElementById(idname);
+    obj.style.backgroundColor = "#000000";
+
+    let text_color = getColor();
+    obj.style.color = text_color;
+    obj.style.fontSize = 1000 + "%";
+    obj.style.textAlign = "center";
+    obj.style.verticalAlign = "middle";
+    let text_meaning_color = getColor();
+    if (text_meaning_color == "#ff0000") {
+        obj.textContent = "赤";
+    } else if (text_meaning_color == "#00ff00") {
+        obj.textContent = "緑";
+    } else if (text_meaning_color == "#0000ff") {
+        obj.textContent = "青";
+    } else {
+        obj.textContent = "Error"
+    }
 }
