5620204dfd4659cfa263f38e5aab767d03ac5654
Yuya Imoto <yuya@yuya-MacBookPro.local>
2020-05-13 16:13:38 +0900

    カウントダウン画面を追加した(画面中央に数字が表示されないので直したい)

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function input(){
 function output(){
     alert("ID : " + localStorage.getItem('id') + ", Name : " + localStorage.getItem('name'));
 }
+
+function countdown(idname){
+    var obj = document.getElementById(idname);
+    obj.style.color = "#ffffff";
+    obj.style.backgroundColor = "#000000";
+    obj.style.fontSize = 1000 + "%";
+    obj.style.textAlign = "center";
+    obj.style.verticalAlign = "middle";
+    var i = 0;
+    setInterval(function(){
+        var counter = 3 - i;
+        obj.textContent = counter;
+        if (counter < 0) {
+            location.href = "./index.html";
+        }
+        i++;
+    }, 1000);
+}
